adc922f50a1072461969c18e9f4c3ed54b15d563
Mustafa Johnny <mustafajohnn99@gmail.com>
2025-07-17 21:42:48 +0300

    nigga

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import compression from "compression";
 import cookieParser from "cookie-parser";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cors from "cors"; // Add this at the top with other imports
 
 dotenv.config();
 
@@ -12,6 +13,17 @@ mongoose.connect(process.env.MONGO).then(() => console.log("MongoDb is connected
 .catch((err) => console.log(err));
 
 const app = e();
+const corsOptions = {
+  origin: [
+    'https://altwebtest-1.onrender.com', 
+    'http://localhost:5173' // For local development
+  ],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+};
+
+app.use(cors(corsOptions)); // Add this before other middleware
 
 app.use(e.json());
 app.use(cookieParser())
