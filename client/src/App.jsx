a6e1b24732ff47c9f4bc6b58aed38d8c578b4497
Mustafa Johnny <mustafajohnn99@gmail.com>
2025-07-18 15:57:21 +0300

    fixx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,19 @@ import Header from "./components/Header";
 import { Toaster } from "react-hot-toast";
 import { Routes, Route } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
+import NonPrivateRoute from "./components/NonPrivateRoute";
 
 export default function App() {
   return (
     <>
       <Header />
       <Routes>
-        <Route path="/" element={<SignUp />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/sign-in" element={<Signin />} />
+        <Route element={<NonPrivateRoute/>}>
+          <Route path="/" element={<SignUp />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<Signin />} />
         <Route element={<PrivateRoute />}>
+        </Route>
           <Route path="/users" element={<Users />} />
         </Route>
       </Routes>
